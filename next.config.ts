1b374805c2bf549dd1ae355360efac294c2ec646
znajdziemy <szyc.ms@gmail.com>
2026-07-09 13:29:45 +0200

    Remove static export for API routes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
   reactStrictMode: false,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
