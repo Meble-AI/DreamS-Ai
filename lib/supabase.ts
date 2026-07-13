71a55607ca32de48b028f412f7112295ba39e815
znajdziemy <szyc.ms@gmail.com>
2026-07-13 07:37:26 +0200

    Fix required login before Stripe checkout

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,20 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase =
-  createClient(
-
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-
-    {
-      auth: {
-
-        persistSession: true,
-
-        autoRefreshToken: false,
-
-        detectSessionInUrl: false,
-      },
-    }
-  );
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
+    },
+  }
+);
